c08e1d1cbe5214358611d7d4ee2c4fdd77715824
Suhajda Tamás <sutajo@gmail.com>
2025-09-14 18:50:56 +0200

    Remove dead code

--- emscripten-rs-macros/src/lib.rs
+++ emscripten-rs-macros/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::{ToTokens, quote};
-use syn::{punctuated::Punctuated, *};
+use syn::*;
 
 #[proc_macro]
 pub fn len_in_bytes(input: TokenStream) -> TokenStream {
@@ -39,26 +39,5 @@ pub fn get_processed_script(input: TokenStream) -> TokenStream {
         [ #( #tokens ),*, b'\0' ]
     };
 
-    output.into()
-}
-
-#[proc_macro]
-pub fn len_params(input: TokenStream) -> TokenStream {
-    let idents = parse_macro_input!(input with Punctuated::<Ident, Token![,]>::parse_terminated);
-
-    // Calculate total length
-    let mut length = 0;
-    for ident in idents.iter() {
-        length += ident.to_string().len();
-    }
-    // Add commas: number of commas = number of identifiers - 1 (if at least one ident exists)
-    if idents.len() > 1 {
-        length += idents.len() - 1;
-    }
-
-    // Return a literal
-    let output = quote! {
-        #length
-    };
     output.into()
 }
